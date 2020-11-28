ba7c804b1382d2f93f5ec0478ef3446298a2f000
Bogdan Noana <bogdan.noana@cst.ro>
2020-11-28 21:58:18 +0200

    init project

---
+++ app/src/main/java/ip/team13/petowner/MainActivity.kt
@@ -0,0 +1,11 @@
+package ip.team13.petowner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
