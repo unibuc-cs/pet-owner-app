36f8f6c85b799c64b8f89836468be14c241e0347
Andrei Mesina <andreimesina98@gmail.com>
2020-12-05 19:30:08 +0200

    Created navigation graph and configured it to use bottom navigation in main screen

--- app/src/main/java/ip/team13/petowner/ui/main/MainActivity.kt
+++ app/src/main/java/ip/team13/petowner/ui/main/MainActivity.kt
@@ -3,6 +3,9 @@ package ip.team13.petowner.ui.main
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ip.team13.petowner.R
 import ip.team13.petowner.databinding.MainScreenBinding
 
@@ -15,5 +18,9 @@ class MainActivity : FragmentActivity() {
 
         val binding = DataBindingUtil.setContentView<MainScreenBinding>(this, R.layout.main_screen)
         binding.viewModel = viewModel
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
 }
