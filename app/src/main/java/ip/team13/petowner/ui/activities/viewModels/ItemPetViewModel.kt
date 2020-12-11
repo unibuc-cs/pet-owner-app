662792a4850f138bcd3ff5a8305df5449871c9ca
Bogdan Noana <bogdan.noana@cst.ro>
2020-12-11 23:07:31 +0200

    fix selected animation

--- app/src/main/java/ip/team13/petowner/ui/activities/viewModels/ItemPetViewModel.kt
+++ app/src/main/java/ip/team13/petowner/ui/activities/viewModels/ItemPetViewModel.kt
@@ -2,11 +2,23 @@ package ip.team13.petowner.ui.activities.viewModels
 
 import androidx.databinding.ObservableField
 import ip.team13.petowner.data.dto.pet.PetDataModel
+import ip.team13.petowner.helpers.addOnPropertyChanged
 
 class ItemPetViewModel (
     val model: PetDataModel,
     val selectedPet: ObservableField<PetDataModel>
 ) {
+
+    val isSelected = ObservableField(false)
+
+    init {
+        selectedPet.addOnPropertyChanged {
+            val newState = it.get() == model
+            if (isSelected.get() != newState) {
+                isSelected.set(newState)
+            }
+        }
+    }
 
     fun onSelect() {
         selectedPet.set(model)
