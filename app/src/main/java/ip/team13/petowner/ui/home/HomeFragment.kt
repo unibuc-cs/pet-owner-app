b51b5e0dc8b580b6055e90dfbaef81e36a144e67
Andrei Mesina <andreimesina98@gmail.com>
2020-12-11 04:20:47 +0200

    Added new components to Home screen

--- app/src/main/java/ip/team13/petowner/ui/home/HomeFragment.kt
+++ app/src/main/java/ip/team13/petowner/ui/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package ip.team13.petowner.ui.home
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.tabs.TabLayoutMediator
 import ip.team13.petowner.R
 import ip.team13.petowner.core.BaseFragment
 import ip.team13.petowner.databinding.HomeScreenBinding
@@ -11,4 +14,19 @@ class HomeFragment : BaseFragment<HomeScreenBinding>() {
         get() = R.layout.home_screen
 
     override val viewModel: HomeViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.homeLeaderboardLayout.homeLeaderboardPager.apply {
+            adapter = HomeLeaderboardAdapter(this@HomeFragment)
+
+            TabLayoutMediator(
+                binding.homeLeaderboardLayout.homeLeaderboardTabs,
+                this
+            ) { tab, position ->
+                tab.text = getString(HomeLeaderboardAdapter.getTabTitle(position))
+            }.attach()
+        }
+    }
 }
