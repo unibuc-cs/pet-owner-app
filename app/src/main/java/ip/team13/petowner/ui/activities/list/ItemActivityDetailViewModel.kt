4cf9b593f3e0e394d0fd0aaf12458ceb768da7e5
MariusMsw <mariusliviu70@yahoo.com>
2020-12-20 16:04:56 +0200

    update edit text on new activity screen

--- app/src/main/java/ip/team13/petowner/ui/activities/list/ItemActivityDetailViewModel.kt
+++ app/src/main/java/ip/team13/petowner/ui/activities/list/ItemActivityDetailViewModel.kt
@@ -1,9 +1,18 @@
 package ip.team13.petowner.ui.activities.list
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.view.View
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.databinding.ObservableField
+import ip.team13.petowner.core.BaseViewModel
+import ip.team13.petowner.core.helpers.addOnPropertyChanged
+import java.text.SimpleDateFormat
+import java.util.*
 
 enum class ItemDetailType {
     TITLE,
@@ -21,17 +30,44 @@ class ItemActivityDetailViewModel(
     private val itemDetailType: ItemDetailType
 ) {
 
-    fun onClick() = when (itemDetailType) {
+    fun onClick(view: View) = when (itemDetailType) {
         ItemDetailType.TITLE -> {
         }
         ItemDetailType.DESCRIPTION -> {
         }
         ItemDetailType.DUE_TIME -> {
+            onShowCalendar(view.context)
         }
         ItemDetailType.REPEAT -> {
         }
         ItemDetailType.REMINDER -> {
         }
     }
+
+    fun getFocusableState() = when (itemDetailType) {
+        ItemDetailType.TITLE, ItemDetailType.DESCRIPTION -> true
+        else -> false
+    }
+
+    private val calendar: Calendar = Calendar.getInstance()
+    private val dateSetListener =
+        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            fieldValue.set(SimpleDateFormat("MM/dd/yyyy", Locale.US).format(calendar.time))
+        }
+
+    private fun onShowCalendar(context: Context) {
+        DatePickerDialog(
+            context,
+            dateSetListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
 }
+
 
