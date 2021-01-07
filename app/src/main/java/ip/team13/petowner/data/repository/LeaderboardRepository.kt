7d44c5574536e9bf36938cc0025846e3997f0d46
Bogdan Noana <nbogdand@yahoo.com>
2021-01-07 18:48:19 +0200

    remove comments

--- app/src/main/java/ip/team13/petowner/data/repository/LeaderboardRepository.kt
+++ app/src/main/java/ip/team13/petowner/data/repository/LeaderboardRepository.kt
@@ -11,43 +11,6 @@ class LeaderboardRepository(
     private val petOwnerAPI: PetOwnerAPI
 ) {
 
-//    fun getTop(type: LeaderboardType) = when (type) {
-//        LeaderboardType.FREE -> arrayListOf(
-//            LeaderboardEntry("John", "", 10),
-//            LeaderboardEntry("Christina", "", 2),
-//            LeaderboardEntry("Michael", "", 7),
-//            LeaderboardEntry("Angelina", "", 8),
-//            LeaderboardEntry("Matt", "", 15),
-//            LeaderboardEntry("Joanna", "", 20),
-//            LeaderboardEntry("Mike", "", 5)
-//        )
-//        LeaderboardType.VIP -> arrayListOf(
-//            LeaderboardEntry("Alexandra", "", 6),
-//            LeaderboardEntry("Chrys", "", 12),
-//            LeaderboardEntry("Sabrina", "", 8),
-//            LeaderboardEntry("Andrew", "", 3),
-//            LeaderboardEntry("Briana", "", 10),
-//            LeaderboardEntry("Fred", "", 15),
-//            LeaderboardEntry("Emily", "", 18)
-//        )
-//    }
-
-//    fun getTop5Vip(): List<LeaderboardEntry> = getTop(LeaderboardType.VIP)
-//        .sortedByDescending { it.weeklyExperience }
-//        .take(5)
-
-//    fun getTop5Free(): List<LeaderboardEntry> = getTop(LeaderboardType.FREE)
-//        .sortedByDescending { it.weeklyExperience }
-//        .take(5)
-
-//    suspend fun getTop5Free(): List<LeaderboardEntry> =
-//        petOwnerAPI.getLeaderboards(
-//            body = LeaderboardRequestModel(
-//                size = 5,
-//                isVIP = false
-//            )
-//        ).map { it.toLeaderboardEntry() }
-
     suspend fun getTop(size: Int, type: LeaderboardType): List<LeaderboardEntry> {
         return try {
             petOwnerAPI.getLeaderboards(
